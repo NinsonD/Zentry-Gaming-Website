68ed2b74b35e56882b5a48968d06c7a1455b85ae
NinsonD <godclutch958@gmail.com>
2025-07-18 20:37:45 +0400

    Update index.html title and enhance About component content and structure

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import AnimatedTitle from "./AnimatedTitle";
+import { ScrollTrigger } from "gsap/all";
 
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import AnimatedTitle from "./AnimatedTitle";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -22,32 +22,36 @@ const About = () => {
     clipAnimation.to(".mask-clip-path", {
       width: "100vw",
       height: "100vh",
-      borderRadius: "0",
+      borderRadius: 0,
     });
   });
 
   return (
-    <div id="about" className="min-h-screen w-screen ">
+    <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-        <h2 className="font-general text-sm uppercase md:text-[10px]">
+        <p className="font-general text-sm uppercase md:text-[10px]">
           Welcome to Zentry
-        </h2>
+        </p>
 
         <AnimatedTitle
-          title="Disc<b>o</b>ver the world's <br/> l<b>a</b>rgest shared adventure"
+          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
           containerClass="mt-5 !text-black text-center"
         />
 
         <div className="about-subtext">
-          <p>The Game of Games begins-you life, now an epic MMORPG</p>
-          <p>Zentry unites every player from countless games and platforms</p>
+          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p className="text-gray-500">
+            Zentry unites every player from countless games and platforms, both
+            digital and physical, into a unified Play Economy
+          </p>
         </div>
       </div>
-      <div id="clip" className="h-dvh w-screen">
+
+      <div className="h-dvh w-screen" id="clip">
         <div className="mask-clip-path about-image">
           <img
             src="img/about.webp"
-            alt="background"
+            alt="Background"
             className="absolute left-0 top-0 size-full object-cover"
           />
         </div>
